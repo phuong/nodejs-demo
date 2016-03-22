7f754c675da12a7f6e67521f1e8559cc5764a5f0
Phuong <boingonline@gmail.com>
2016-03-22 15:29:03 +0700

    Fix bugs

--- test.js
+++ test.js
@@ -2,3 +2,20 @@ var Promise = require('promise');
 var DB = require('./lib/db');
 var Like = require('./lib/like');
 var Product = require('./lib/product');
+
+
+data1 = {
+  productId: 1,
+  userId: 1,
+}
+
+data2 = {
+  productId: 1,
+  userId: 1,
+  makeException: true
+}
+
+Like.save(data1, function () {
+  DB.close();
+})
+
